2bda003980359567c2ba026e00a252e0b83bf245
Joe Reiff <jcreiff@gmail.com>
2018-01-18 11:46:32 -0500

    complete raindrops tests

---
+++ ruby/raindrops/raindrops.rb
@@ -0,0 +1,14 @@
+class Raindrops
+
+  def self.convert(num, drops=[])
+    [3,5,7].map{|factor| drops << NOISES[factor] if num%factor==0}
+    drops==[] ? num.to_s : drops.join("")
+  end
+
+  NOISES={3=>"Pling", 5=>"Plang", 7=>"Plong"}
+
+end
+
+module BookKeeping
+  VERSION=3
+end
