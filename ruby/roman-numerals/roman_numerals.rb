49daba0a15b4e06d928288e556f19f9b561c8947
Joe Reiff <jcreiff@gmail.com>
2018-01-18 13:22:16 -0500

    complete roman numeral tests

---
+++ ruby/roman-numerals/roman_numerals.rb
@@ -0,0 +1,22 @@
+DIGITS = { 1000=>"M", 900=>"CM", 500=>"D", 400=>"CD", 100=>"C", 90=>"XC",
+  50=>"L",  40=>"XL", 10=>"X", 9=>"IX", 5=>"V", 4=>"IV", 1=>"I"}
+
+class Fixnum
+
+  def to_roman
+    string = ""
+    total = self
+    DIGITS.each do |arabic, roman|
+      dividend, remainder = total.divmod(arabic)
+      next if dividend == 0
+      string += roman * dividend
+      total = remainder
+    end
+    string
+  end
+
+end
+
+module BookKeeping
+  VERSION = 2
+end
