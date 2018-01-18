ae2011f4a7e07cd5da20b05e5b85658ce822f178
Joe Reiff <jcreiff@gmail.com>
2018-01-18 12:26:55 -0500

    complete pangram tests

---
+++ ruby/pangram/pangram.rb
@@ -0,0 +1,15 @@
+require 'set'
+
+class Pangram
+  def self.pangram?(phrase)
+    phrase.downcase!
+    ("a".."z").to_a.each do |letter|
+      phrase.count(letter) == 0 ? return : next
+    end
+    return true
+  end
+end
+
+module BookKeeping
+  VERSION = 6
+end
