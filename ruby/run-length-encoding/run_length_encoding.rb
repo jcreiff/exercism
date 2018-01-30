075945e6cb92f1335ba56c5f11ce5851d00687e2
Joe Reiff <jcreiff@gmail.com>
2018-01-30 15:48:00 -0500

    shorter solution, but doesn't yet satisfy repeated character tests

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,64 +1,14 @@
 class RunLengthEncoding
-  attr_reader :chars
-
-  def initialize
-    @chars = []
-  end
-
-  def encode(input)
-    split_chars(input.chars)
-    combine_chars
-  end
-
-  def split_chars(input, index = 0)
-    return chars if input == []
-    index += 1 until input[index] != input[index + 1]
-    chars << input[0..index].join
-    input.slice!(0, index + 1)
-    split_chars(input)
-  end
-
-  def combine_chars
-    counts = chars.map { |char_set| char_set.length.to_s }.map { |count| count == '1' ? '' : count }
-    letters = chars.map { |char_set| char_set[0] }
-    counts.zip(letters).join
-  end
-
-  def decode(input)
-    split_nums_and_chars(input)
-    write_out_chars
-  end
-
-  def split_nums_and_chars(input, index = 0)
-    return chars if input == ''
-    if input[index].to_i.zero?
-      chars << input[index]
-    else
-      index += 1 until input[index + 1].to_i.zero?
-      chars << input[0..index].to_i
-    end
-    input.slice!(0, index + 1)
-    split_nums_and_chars(input)
-  end
-
-  def write_out_chars(index = 0, string = '')
-    return string if chars[index].nil?
-    if chars[index].class == Fixnum
-      string += (chars[index + 1] * chars[index])
-      index += 2
-    else
-      string += chars[index]
-      index += 1
-    end
-    write_out_chars(index, string)
-  end
-
   def self.encode(input)
-    RunLengthEncoding.new.encode(input)
+    chars = input.squeeze.chars
+    counts = chars.map { |char| input.count(char) }
+    counts.zip(chars).flatten.reject { |char| char == 1 }.join
   end
 
   def self.decode(input)
-    RunLengthEncoding.new.decode(input)
+    separated = input.split(/(\D)/)
+    sets = separated.slice_after { |x| x.to_i == 0}.reject { |set| set == ['']}
+    sets.map { |set| set[1].nil? ? set[0] : set[1] * set[0].to_i }.join
   end
 end
 
