3de6ff287a158056eeb2fddb10d98c2b030b9f28
Joe Reiff <jcreiff@gmail.com>
2018-01-30 16:16:53 -0500

    all rle tests passing with refactor

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,13 +1,17 @@
 class RunLengthEncoding
   def self.encode(input)
-    chars = input.squeeze.chars
-    counts = chars.map { |char| input.count(char) }
-    counts.zip(chars).flatten.reject { |char| char == 1 }.join
+    chars = input.chars
+    next_index = 0
+    groups = chars.slice_after do |char|
+      next_index += 1
+      char != chars[next_index]
+    end
+    groups.map { |g| (g.count == 1 ? '' : g.count.to_s) + g[0] }.join
   end
 
   def self.decode(input)
     separated = input.split(/(\D)/)
-    sets = separated.slice_after { |x| x.to_i == 0}.reject { |set| set == ['']}
+    sets = separated.slice_after { |char| char.to_i.zero? }.reject(&:empty?)
     sets.map { |set| set[1].nil? ? set[0] : set[1] * set[0].to_i }.join
   end
 end
