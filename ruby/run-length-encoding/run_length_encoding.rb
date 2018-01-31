e4ff43c67ff21181617ae7361425afcaf772d632
Joe Reiff <jcreiff@gmail.com>
2018-01-31 15:08:23 -0500

    updated methods with better regex scan and chunk

--- ruby/run-length-encoding/run_length_encoding.rb
+++ ruby/run-length-encoding/run_length_encoding.rb
@@ -1,18 +1,14 @@
 class RunLengthEncoding
   def self.encode(input)
-    chars = input.chars
-    next_index = 0
-    groups = chars.slice_after do |char|
-      next_index += 1
-      char != chars[next_index]
-    end
-    groups.map { |g| (g.count == 1 ? '' : g.count.to_s) + g[0] }.join
+    input.chars.chunk(&:itself).map do |char, set|
+      [(set.count == 1 ? '' : set.count), char]
+    end.join
   end
 
   def self.decode(input)
-    separated = input.split(/(\D)/)
-    sets = separated.slice_after { |char| char.to_i.zero? }.reject(&:empty?)
-    sets.map { |set| set[1].nil? ? set[0] : set[1] * set[0].to_i }.join
+    input.scan(/(\d*)(\D)/).map do |num, char|
+      char * (num.empty? ? 1 : num.to_i)
+    end.join
   end
 end
 
