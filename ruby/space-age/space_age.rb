a1af9ce33ea3776b27ad38310721d455dcf26089
Joe Reiff <jcreiff@gmail.com>
2018-02-04 21:23:30 -0500

    replace method_missing with define_method

--- ruby/space-age/space_age.rb
+++ ruby/space-age/space_age.rb
@@ -1,18 +1,18 @@
 class SpaceAge
-  attr_reader :on_earth
-
   def initialize(seconds)
-    @on_earth = seconds / 31_557_600.0
+    @seconds = seconds
   end
 
-  def method_missing(planet)
-    on_earth / ORBIT_LENGTHS[planet]
-  end
+  EARTH_YEAR = 31_557_600.0
 
-  ORBIT_LENGTHS = { on_mercury: 0.2408467, on_venus: 0.61519726,
+  ORBIT_LENGTHS = { on_mercury: 0.2408467, on_venus: 0.61519726, on_earth: 1.0,
                     on_mars: 1.8808158, on_jupiter: 11.862615,
                     on_saturn: 29.447498, on_uranus: 84.016846,
-                    on_neptune: 164.79132 }
+                    on_neptune: 164.79132 }.freeze
+
+  ORBIT_LENGTHS.each do |planet, length|
+    define_method(planet) { @seconds / EARTH_YEAR / length }
+  end
 end
 
 module BookKeeping
