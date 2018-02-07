6950786ac148ccd43ca95225d9f2b18b6b74c87b
Joe Reiff <jcreiff@gmail.com>
2018-02-07 15:48:32 -0500

    complete scrabble tests

---
+++ ruby/scrabble-score/scrabble_score.rb
@@ -0,0 +1,19 @@
+class Scrabble
+  def initialize(word)
+    @word = word
+  end
+
+  def score
+    return 0 unless @word =~ /\w/
+    @word.upcase.chars.map { |letter| LETTER_SCORES[letter] }.reduce(:+)
+  end
+
+  def self.score(word)
+    Scrabble.new(word).score
+  end
+
+  LETTER_SCORES = Hash.new(1).merge('D' => 2, 'G' => 2, 'B' => 3, 'C' => 3,
+                                    'M' => 3, 'P' => 3, 'F' => 4, 'H' => 4,
+                                    'V' => 4, 'W' => 4, 'Y' => 4, 'K' => 5,
+                                    'J' => 8, 'X' => 8, 'Q' => 10, 'Z' => 10)
+end
