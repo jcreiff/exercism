5874689704195e8e35e0efe0d9ba203a04b1a186
Joe Reiff <jcreiff@gmail.com>
2018-02-07 17:14:16 -0500

    refactored to explicit implementation

--- ruby/flatten-array/flatten_array.rb
+++ ruby/flatten-array/flatten_array.rb
@@ -1,6 +1,8 @@
 class FlattenArray
   def self.flatten(array)
-    array.flatten.compact
+    array.reject(&:nil?).each_with_object([]) do |item, output|
+      item.class == Array ? output.concat(flatten(item)) : output << item
+    end
   end
 end
 
