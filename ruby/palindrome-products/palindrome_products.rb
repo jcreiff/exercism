28c3b25be92916075fb6533673a6b0fed9231971
Joe Reiff <jcreiff@gmail.com>
2018-02-12 18:12:59 -0500

    another solution, but not really any better except for define_method

--- ruby/palindrome-products/palindrome_products.rb
+++ ruby/palindrome-products/palindrome_products.rb
@@ -7,30 +7,38 @@ module Palindromic
 end
 
 class Palindromes
-  def initialize(min_factor: 1, max_factor:)
-    @min_factor = min_factor
-    @max_factor = max_factor
-    @range = (min_factor..max_factor).to_a
-    @set = Hash.new { |h, k| h[k] = [] }
-  end
+  attr_reader :smallest, :largest
 
   using Palindromic
+  def initialize(min_factor: 1, max_factor:)
+    @factor_range = (min_factor..max_factor).to_a
+    @pal_range = (min_factor**2..max_factor**2).to_a.select(&:palindrome?)
+    @smallest = { value: nil, factors: [] }
+    @largest = { value: nil, factors: [] }
+  end
+
   def generate
-    pals = @range.repeated_permutation(2).select { |pair| pair.reduce(:*).palindrome? }
-    pals.map { |pal| @set[pal.reduce(:*)].push(pal.sort) }
+    low = @pal_range.find { |pal| divisible_in_range(pal) }
+    high = @pal_range.reverse.find { |pal| divisible_in_range(pal) }
+    smallest[:value] = low
+    largest[:value] = high
+    smallest[:factors] = find_factors(low).uniq
+    largest[:factors] = find_factors(high).uniq
   end
 
-  def largest
-    { value: @set.keys.max, factors: @set[@set.keys.max].uniq }
+  private
+
+  def divisible_in_range(pal)
+    @factor_range.any? { |num| (pal % num).zero? && @factor_range.include?(pal / num) }
   end
 
-  def smallest
-    { value: @set.keys.min, factors: @set[@set.keys.min].uniq }
+  def find_factors(pal)
+    @factor_range.select { |factor| (pal % factor).zero? }.map { |factor| [factor, pal/factor].sort }.reject { |set| set.any? { |num| !@factor_range.include?(num) } }
   end
 end
 
 class Hash
-  def method_missing(name)
-    self[name]
+  %i[value factors].each do |prop|
+    define_method(prop) { self[prop] }
   end
 end
