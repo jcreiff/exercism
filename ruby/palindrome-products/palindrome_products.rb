dbf10d6ceb268d9e227601df0a115fc86bb5ac9c
Joe Reiff <jcreiff@gmail.com>
2018-02-12 20:55:02 -0500

    slightly faster/more readable version

--- ruby/palindrome-products/palindrome_products.rb
+++ ruby/palindrome-products/palindrome_products.rb
@@ -6,39 +6,28 @@ module Palindromic
   end
 end
 
-class Palindromes
-  attr_reader :smallest, :largest
-
-  using Palindromic
-  def initialize(min_factor: 1, max_factor:)
-    @factor_range = (min_factor..max_factor).to_a
-    @pal_range = (min_factor**2..max_factor**2).to_a.select(&:palindrome?)
-    @smallest = { value: nil, factors: [] }
-    @largest = { value: nil, factors: [] }
-  end
-
-  def generate
-    low = @pal_range.find { |pal| divisible_in_range(pal) }
-    high = @pal_range.reverse.find { |pal| divisible_in_range(pal) }
-    smallest[:value] = low
-    largest[:value] = high
-    smallest[:factors] = find_factors(low).uniq
-    largest[:factors] = find_factors(high).uniq
-  end
-
-  private
-
-  def divisible_in_range(pal)
-    @factor_range.any? { |num| (pal % num).zero? && @factor_range.include?(pal / num) }
-  end
-
-  def find_factors(pal)
-    @factor_range.select { |factor| (pal % factor).zero? }.map { |factor| [factor, pal/factor].sort }.reject { |set| set.any? { |num| !@factor_range.include?(num) } }
-  end
+class Hash
+  %i[value factors].each { |prop| define_method(prop) { self[prop] } }
 end
 
-class Hash
-  %i[value factors].each do |prop|
-    define_method(prop) { self[prop] }
+class Palindromes
+  def initialize(min_factor: 1, max_factor:)
+    @range = (min_factor..max_factor).to_a
+    @collection = Hash.new { |h, k| h[k] = { value: k, factors: [] } }
+  end
+
+  def largest
+    @collection.max.last
+  end
+
+  def smallest
+    @collection.min.last
+  end
+
+  using Palindromic
+  def generate
+    @range.repeated_combination(2)
+          .partition { |set| set.reduce(:*).palindrome? }.first
+          .each { |pal| @collection[pal.reduce(:*)][:factors] << pal }
   end
 end
