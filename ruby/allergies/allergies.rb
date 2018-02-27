15e0aadfd9a0fe07eb6b8871acc1ed0aed1f4368
Joe Reiff <jcreiff@gmail.com>
2018-02-26 19:18:45 -0500

    refactored attempt at allergies tests

--- ruby/allergies/allergies.rb
+++ ruby/allergies/allergies.rb
@@ -1,41 +1,31 @@
 class Allergies
-  attr_accessor :score, :score_list, :list
+  attr_reader :list
 
   def initialize(score)
-    @score = score
-    @score_list = parse_score
-    @list = map_scores
+    @score = score > 255 ? adjust(score) : score
+    @all_allergies = parse_score(@score)
+    @list = @all_allergies.select { |_h, k| k }.keys.reverse
   end
 
   def allergic_to?(allergen)
-    list.include?(allergen)
+    @all_allergies[allergen]
   end
 
-  def parse_score(allergen_list = [])
-    reject_unlisted_allergens if score > 255
-    (0..7).to_a.map { |i| 2**i }.reverse.each do |num|
-      if score - num >= 0
-        allergen_list.unshift(num)
-        self.score -= num
-      end
-    end
-    allergen_list
-  end
-
-  def map_scores
-    score_list.map { |score| ALLERGENS[score] }
-  end
-
-  def reject_unlisted_allergens(upper_bound = 9)
-    upper_bound += 1 until 2**upper_bound > score
-    (8..upper_bound).to_a.map { |i| 2**i }.reverse.each do |num|
-      self.score -= num if score - num >= 0
+  def parse_score(score)
+    ALLERGENS.each_with_object({}) do |(number, allergen), hash|
+      hash[allergen] = score - number >= 0
+      score -= number if hash[allergen]
     end
   end
 
-  ALLERGENS = { 1 => 'eggs', 2 => 'peanuts', 4 => 'shellfish',
-                8 => 'strawberries', 16 => 'tomatoes', 32 => 'chocolate',
-                64 => 'pollen', 128 => 'cats' }.freeze
+  def adjust(score, limit = 8)
+    limit += 1 until 2**limit > score
+    limit.downto(8).map { |i| 2**i }.each { |n| score -= n if score - n >= 0 }
+    score
+  end
+
+  ALLERGENS = Hash[128, 'cats', 64, 'pollen', 32, 'chocolate', 16, 'tomatoes',
+                   8, 'strawberries', 4, 'shellfish', 2, 'peanuts', 1, 'eggs']
 end
 
 module BookKeeping
