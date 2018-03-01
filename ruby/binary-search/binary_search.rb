f89165ae4442c6212c4249c90584dfb77d35d954
Joe Reiff <jcreiff@gmail.com>
2018-03-01 11:47:18 -0500

    use spaceshp operator

--- ruby/binary-search/binary_search.rb
+++ ruby/binary-search/binary_search.rb
@@ -12,16 +12,18 @@ class BinarySearch
   end
 
   def search_for(item, position = middle, search_index = middle, search_list = list.dup)
-    return position if item == search_list[search_index]
     raise RuntimeError if search_list == []
-    if item > search_list[search_index]
+    result = item <=> search_list[search_index]
+    if result == 1
       search_list.slice!(0..search_index)
       search_index = find_middle(search_list)
       position += search_index + 1
-    else
+    elsif result == -1
       search_list.slice!(search_index..-1)
       search_index = find_middle(search_list)
       search_list.count.even? ? position -= search_index : position -= search_index + 1
+    else
+      return position
     end
     search_for(item, position, search_index, search_list)
   end
