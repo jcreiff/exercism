0617be27711258bd7b03b08d8c8a9665d9efcfc0
Joe Reiff <jcreiff@gmail.com>
2018-03-05 21:45:45 -0500

    refactored diamond

--- ruby/diamond/diamond.rb
+++ ruby/diamond/diamond.rb
@@ -1,33 +1,32 @@
 class Diamond
-  attr_reader :middle_letter, :middle_row, :width
+  attr_reader :middle_letter, :middle_row
 
   def initialize(middle_letter)
     @middle_letter = middle_letter
-    @middle_row = make_row(middle_letter)
-    @width = middle_row.length
+    @middle_row = row(middle_letter)
   end
 
-  def make_diamond(top = [])
+  def make_diamond
     return "A\n" if middle_letter == 'A'
-    ('A'...middle_letter).each do |letter|
-      inside = make_row(letter)
-      outer = ' ' * ((width - inside.length) / 2)
-      top << outer + inside + outer
-    end
-    top.join("\n") + "\n#{middle_row}\n" + top.reverse.join("\n") + "\n"
+    [half, middle_row, half.reverse].join("\n") + "\n"
   end
-
-  def make_row(letter)
-    return letter if letter == 'A'
-    inner_space = (LETTERS.index(letter) * 2) - 1
-    letter + (' ' * inner_space) + letter
-  end
-
-  LETTERS = ('A'..'Z').to_a
 
   def self.make_diamond(letter)
     Diamond.new(letter).make_diamond
   end
+
+  private
+
+  def half
+    ('A'...middle_letter).map { |letter| row(letter).center(@middle_row.size) }
+  end
+
+  def row(letter)
+    return letter if letter == 'A'
+    letter + (' ' * (ALPHA.index(letter) * 2 - 1)) + letter
+  end
+
+  ALPHA = ('A'..'Z').to_a
 end
 
 module Bookkeeping
