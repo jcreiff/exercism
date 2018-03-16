f04bd2c385bc2da9fa2379676d97ad216b553e2d
Joe Reiff <jcreiff@gmail.com>
2018-03-16 10:01:00 -0400

    complete gigasecond tests

---
+++ javascript/gigasecond/gigasecond.js
@@ -0,0 +1,9 @@
+var Gigasecond = function(birth) {
+  this.birth = birth.getTime();
+};
+
+Gigasecond.prototype.date = function() {
+  return new Date((this.birth + 1000000000000));
+};
+
+module.exports = Gigasecond;
