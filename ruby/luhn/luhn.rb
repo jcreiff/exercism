2468d03e4369c94294ec5207dc03b6f1aa1a8617
Joe Reiff <jcreiff@gmail.com>
2020-03-18 14:50:41 -0400

    refactored

--- ruby/luhn/luhn.rb
+++ ruby/luhn/luhn.rb
@@ -1,29 +1,30 @@
 class Luhn
   def initialize(number)
-    @raw_number = number
-    @number = number.scan(/\d/).map(&:to_i).reverse
+    @number = number
   end
 
   def valid?
-    @number.size > 1 && @raw_number.delete(' ') !~ /\D/ && (checksum % 10).zero?
+    digits = valid_input
+    digits && (checksum(digits) % 10).zero?
   end
 
   def self.valid?(num)
-    Luhn.new(num).valid?
+    new(num).valid?
   end
 
   private
 
-  def addends
-    @number.map.with_index { |num, i| i.odd? ? replace(num) : num }
+  def valid_input
+    input = @number.dup.delete(' ')
+    input !~ /\D/ && input.size > 1 ? input.to_i.digits : nil
   end
 
   def replace(num)
-    num < 5 ? num * 2 : num * 2 + 1
+    num < 5 ? num * 2 : num * 2 - 9
   end
 
-  def checksum
-    addends.reduce(:+)
+  def checksum(input)
+    input.map.with_index { _2.odd? ? replace(_1) : _1 }.sum
   end
 end
 
