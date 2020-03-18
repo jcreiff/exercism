8c7f1dc44cb1303b22dad47f0f221fe3d8d66128
Joe Reiff <jcreiff@gmail.com>
2020-03-18 13:28:06 -0400

    refactored to remove extra default arg

--- ruby/raindrops/raindrops.rb
+++ ruby/raindrops/raindrops.rb
@@ -1,12 +1,8 @@
 class Raindrops
-  def self.convert(num, drops = [])
-    [3, 5, 7].map { |factor| drops << NOISES[factor] if (num % factor).zero? }
-    drops == [] ? num.to_s : drops.join
+  def self.convert(num)
+    result = NOISES.select { |h, _k| (num % h).zero? }
+    result.empty? ? num.to_s : result.values.join
   end
 
-  NOISES = { 3 => :Pling, 5 => :Plang, 7 => :Plong }
-end
-
-module BookKeeping
-  VERSION = 3
+  NOISES = { 3 => 'Pling', 5 => 'Plang', 7 => 'Plong' }.freeze
 end
