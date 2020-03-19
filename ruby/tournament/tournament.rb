0c4824d791ba4214925ef45c19179feec8da5609
Joe Reiff <jcreiff@gmail.com>
2020-03-19 11:02:35 -0400

    refactored to make Team a struct, eliminate Game class

--- ruby/tournament/tournament.rb
+++ ruby/tournament/tournament.rb
@@ -1,91 +1,59 @@
 class Tournament
   def initialize(results)
-    @results = results.lines.map(&:chomp).map { |line| line.split(';') }
-    @teams = @results.flat_map { |result| result[0, 2] }
-                     .uniq.map { |name| Team.new(name) }
-    @games = @results.map { |t1, t2, result| Game.new(t1, t2, result) }
+    @results = results.split("\n").map { _1.split(';') }
+    @teams = {}
+    update_results
   end
 
   def tally
-    return Scoreboard::HEADER if @teams.empty?
-    TournamentScorer.new(@games, @teams).score_games
-    Scoreboard.display(@teams)
+    score_games(@results)
+    Scoreboard.display(@teams.values)
   end
 
   def self.tally(results)
     Tournament.new(results).tally
   end
-end
 
-class Team
-  attr_reader :name
-  attr_accessor :wins, :losses, :draws
+  private
 
-  def initialize(name)
-    @name = name
-    @wins = 0
-    @losses = 0
-    @draws = 0
+  def update_results
+    team_names = @results.flatten.uniq.reject { %w[win loss draw].include?(_1) }
+    team_names.map { @teams[_1] = Team.new(_1, 0, 0, 0, 0, 0) }
+    @results.map! { |t1, t2, result| [@teams[t1], @teams[t2], result] }
   end
 
-  def matches_played
-    wins + losses + draws
-  end
-
-  def points
-    wins * 3 + draws
-  end
-end
-
-class Game
-  attr_reader :teams, :result
-
-  def initialize(team1, team2, result)
-    @teams = [team1, team2]
-    @result = result
-  end
-
-  [[:winner, 'win'], [:loser, 'loss']].each do |name, outcome|
-    define_method(name) { result == outcome ? @teams.first : @teams.last }
-  end
-end
-
-class TournamentScorer
-  def initialize(games, teams)
-    @games = games
-    @teams = teams
-  end
-
-  def score_games
-    @games.each do |game|
-      if game.result == 'draw'
-        game.teams.each { |name| @teams.find { |t| t.name == name }.draws += 1 }
+  def score_games(games)
+    games.each do |t1, t2, result|
+      if result == 'draw'
+        [t1, t2].each { score_draws(_1) }
       else
-        @teams.find { |team| team.name == game.winner }.wins += 1
-        @teams.find { |team| team.name == game.loser }.losses += 1
+        score_wins(result == 'win' ? t1 : t2)
+        score_losses(result == 'loss' ? t1 : t2)
       end
+    end
+  end
+
+  [[:wins, 3], [:draws, 1], [:losses, 0]].each do |outcome, points|
+    define_method("score_#{outcome}") do |team|
+      team[:matches] += 1
+      team[outcome] += 1
+      team[:points] += points
     end
   end
 end
 
+Team = Struct.new(:name, :wins, :losses, :draws, :matches, :points)
+
 class Scoreboard
-  HEADER = "Team                           | MP |  W |  D |  L |  P\n".freeze
-  DIVIDER = ' |  '.freeze
+  HEADER = %w[Team MP W D L P].freeze
+  FORMAT = "%-31s| %2s |  %s |  %s |  %s |  %s\n".freeze
 
   def self.display(teams)
-    HEADER + sort(teams).map { |team| stat_line(team) }.join("\n") + "\n"
+    (FORMAT % HEADER) + teams.sort_by { [-_1.points, _1.name] }
+                             .map { FORMAT % stats(_1) }.join
   end
 
-  def self.sort(teams)
-    teams.sort_by { |team| [-team.points, team.name] }
+  def self.stats(team)
+    [team.name, team.matches, team.wins, team.draws, team.losses, team.points]
   end
-
-  def self.stat_line(team)
-    [team.name.ljust(30), team.matches_played, team.wins, team.draws,
-     team.losses, team.points].join(DIVIDER)
-  end
-end
-
-module BookKeeping
-  VERSION = 3
 end
