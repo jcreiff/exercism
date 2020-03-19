5e4d22a775f771566da0de91ecbe178e70f0c742
Joe Reiff <jcreiff@gmail.com>
2020-03-19 18:22:53 -0400

    using #tr

--- ruby/rna-transcription/rna_transcription.rb
+++ ruby/rna-transcription/rna_transcription.rb
@@ -1,12 +1,6 @@
 class Complement
   def self.of_dna(strand)
-    transcribed = strand.chars.map { |base| PAIRS[base] }.join
-    transcribed.length == strand.length ? transcribed : ''
+    return '' if strand =~ /[^CGTA]/
+    strand.tr('CGTA', 'GCAU')
   end
-
-  PAIRS = { 'C' => 'G', 'G' => 'C', 'T' => 'A', 'A' => 'U' }
-end
-
-module BookKeeping
-  VERSION = 4
 end
