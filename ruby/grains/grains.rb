b352295d5ffcf3af083d7463731597f7a8b1241c
Joe Reiff <jcreiff@gmail.com>
2020-03-19 18:07:24 -0400

    minor updates

--- ruby/grains/grains.rb
+++ ruby/grains/grains.rb
@@ -1,15 +1,13 @@
 class Grains
-  def self.square(num)
-    raise ArgumentError, "Number of squares can't be less than 1" if num < 1
-    raise ArgumentError, "Number of squares can't be greater than 64" if num > 64
-    2**(num - 1)
-  end
+  VALID_NUMBERS = (1..64).freeze
+  class<<self
+    def square(num)
+      raise ArgumentError, 'Invalid number' unless VALID_NUMBERS.include?(num)
+      2.pow(num - 1)
+    end
 
-  def self.total
-    (0..63).to_a.map { |i| 2**i }.reduce(:+)
+    def total
+      VALID_NUMBERS.sum { square(_1) }
+    end
   end
-end
-
-module BookKeeping
-  VERSION = 1
 end
