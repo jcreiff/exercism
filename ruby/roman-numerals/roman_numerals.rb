d84a4a84a9e34480b0c2600018d33ad750f295d7
Joe Reiff <jcreiff@gmail.com>
2020-03-19 21:20:25 -0400

    refactored to use module and each_with_object

--- ruby/roman-numerals/roman_numerals.rb
+++ ruby/roman-numerals/roman_numerals.rb
@@ -1,20 +1,16 @@
-DIGITS = { 1000=>'M', 900=>'CM', 500=>'D', 400=>'CD', 100=>'C', 90=>'XC',
-           50=>'L',  40=>'XL', 10=>'X', 9=>'IX', 5=>'V', 4=>'IV', 1=>'I' }
+module Roman
+  DIGITS = { 1000 => 'M', 900 => 'CM', 500 => 'D', 400 => 'CD', 100 => 'C',
+             90 => 'XC', 50 => 'L', 40 => 'XL', 10 => 'X', 9 => 'IX', 5 => 'V',
+             4 => 'IV', 1 => 'I' }.freeze
 
-class Fixnum
   def to_roman
-    string = ''
-    total = self
-    DIGITS.each do |arabic, roman|
+    total = dup
+    DIGITS.each_with_object('') do |(arabic, roman), result|
       dividend, remainder = total.divmod(arabic)
-      next if dividend.zero?
-      string += roman * dividend
+      result << roman * dividend
       total = remainder
     end
-    string
   end
 end
 
-module BookKeeping
-  VERSION = 2
-end
+Integer.include(Roman)
