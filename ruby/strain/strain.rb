30c4d97cacedb6b2d777ea447e43d23fb248ae3b
Joe Reiff <jcreiff@gmail.com>
2020-03-22 16:58:40 -0400

    solved with alias

--- ruby/strain/strain.rb
+++ ruby/strain/strain.rb
@@ -1,17 +1,4 @@
-module Strain
-  def method_missing(name, *args, &block)
-    if name == :keep
-      partition(&block)[0]
-    elsif name == :discard
-      partition(&block)[1]
-    else
-      super
-    end
-  end
-
-  def respond_to_missing?(method, *args, &block)
-    %i[keep discard].include?(method) ? true : super
-  end
+class Array
+  alias keep select
+  alias discard reject
 end
-
-Array.include(Strain)
