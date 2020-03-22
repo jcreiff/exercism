bae94f3767dd032c0832f7837ceedfd7de394058
Joe Reiff <jcreiff@gmail.com>
2020-03-22 19:53:30 -0400

    different solution with refinements

--- ruby/beer-song/beer_song.rb
+++ ruby/beer-song/beer_song.rb
@@ -1,37 +1,43 @@
-class BeerSong
-  def initialize
-    @irregulars = { 1 => '1 bottle', 0 => 'no more bottles', -1 => '99 bottles' }
-    @bottles = Hash.new { |h, k| h[k] = k.to_s + ' bottles' }.merge(@irregulars)
-  end
-
-  def verse(n)
-    first(@bottles[n]) + penultimate(n) + final(@bottles[n - 1])
-  end
-
-  def verses(first, last)
-    (last..first).map { |n| verse(n) }.reverse.join("\n")
-  end
-
-  private
-
-  def first(count)
-    "#{count.capitalize} of beer on the wall, #{count} of beer.\n"
-  end
-
-  def penultimate(n)
-    return out_of_beer if n.zero?
-    "Take #{n == 1 ? 'it' : 'one'} down and pass it around, "
-  end
-
-  def out_of_beer
-    'Go to the store and buy some more, '
-  end
-
-  def final(count)
-    "#{count} of beer on the wall.\n"
+module BeerNumbers
+  refine Integer do
+    def to_b
+      case self
+      when 1
+        '1 bottle of beer'
+      when 0
+        'no more bottles of beer'
+      else
+        "#{self} bottles of beer"
+      end
+    end
   end
 end
 
-module BookKeeping
-  VERSION = 3
+
+class BeerSong
+  class<<self
+    def verse(n)
+      [bookends(n, false), middle(n), bookends(n - 1, true)].join(', ')
+    end
+
+    def recite(first, last)
+      (first - last + 1..first).map { verse(_1) }.reverse.join("\n")
+    end
+
+    private
+
+    using BeerNumbers
+    def bookends(n, final)
+      lyric = "#{[*0..99][n].to_b} on the wall"
+      final ? lyric << ".\n" : lyric.capitalize
+    end
+
+    def middle(n)
+      "#{n.to_b}.\n" << connector(n)
+    end
+
+    def connector(n)
+      n.zero? ? 'Go to the store and buy some more' : "Take #{n == 1 ? 'it' : 'one'} down and pass it around"
+    end
+  end
 end
