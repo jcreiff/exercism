0dc06078d944b2c1006b088dae1b6e1c2bc884f5
Joe Reiff <jcreiff@gmail.com>
2020-03-22 16:44:15 -0400

    updated to make regex do all the work

--- ruby/phone-number/phone_number.rb
+++ ruby/phone-number/phone_number.rb
@@ -1,31 +1,5 @@
 class PhoneNumber
-  attr_accessor :input
-
-  def initialize(input)
-    @input = input.gsub(/\D/, '')
-  end
-
-  def clean
-    return nil unless valid_length?
-    self.input = input[1..-1] if input.length == 11
-    valid_construction? ? input : nil
-  end
-
   def self.clean(input)
-    PhoneNumber.new(input).clean
+    input.gsub(/\D/, '').match(/^1?([2-9]\d{2}[2-9]\d{2}\d{4})$/) { $1 }
   end
-
-  private
-
-  def valid_length?
-    input.length == 10 || input.length == 11 && input[0] == '1'
-  end
-
-  def valid_construction?
-    [input[0], input[3]].all? { |digit| ('2'..'9').cover?(digit) }
-  end
-end
-
-module BookKeeping
-  VERSION = 2
 end
