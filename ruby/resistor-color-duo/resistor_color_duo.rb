7931ce00b55078aa7f2bb052c7f3804db1b666ba
Joe Reiff <jcreiff@gmail.com>
2020-03-24 16:20:43 -0400

    removed splat args

--- ruby/resistor-color-duo/resistor_color_duo.rb
+++ ruby/resistor-color-duo/resistor_color_duo.rb
@@ -1,8 +1,7 @@
 class ResistorColorDuo
-
-  def self.value(*args)
-    args[0].take(2).map { |i| COLORS.index(i) }.join.to_i
-  end
-
   COLORS = %w[black brown red orange yellow green blue violet grey white].freeze
+
+  def self.value(bands)
+    bands.take(2).map { |i| COLORS.index(i) }.join.to_i
+  end
 end
