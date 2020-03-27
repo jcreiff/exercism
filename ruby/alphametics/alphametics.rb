9fa7a7b881522f8d6345e0e4302d16d71f284465
Joe Reiff <jcreiff@gmail.com>
2020-03-27 09:02:31 -0400

    slightly faster but not optimal solution yet

--- ruby/alphametics/alphametics.rb
+++ ruby/alphametics/alphametics.rb
@@ -3,16 +3,16 @@ class Alphametics
 
   def initialize(puzzle)
     @words = puzzle.scan(/\b\w+\b/)
-    @letters = puzzle.scan(/\w/).uniq.sort
+    @letters = puzzle.scan(/\w/).uniq.join
     @leading_digits = get_indexes(0)
     @ones = get_indexes(-1)
     @carryover = words[0..-2].all? { |word| word.length < words.last.length }
   end
 
   def solve
-    to_h(options.reject { |set| leading_zero?(set) }
-                .select { |set| valid_ones?(set) }
-                .find { |set| solution?(set) })
+    to_h((0..9).to_a.permutation(letters.size).find do |set|
+      valid_carryover?(set) && valid_ones?(set) && !leading_zero?(set) && solution?(set)
+    end)
   end
 
   def self.solve(puzzle)
@@ -27,32 +27,24 @@ class Alphametics
 
   def to_h(values)
     return {} if values.nil?
-    letters.zip(values).to_h
-  end
-
-  def options
-    carryover ? all_combos.select { |set| valid_carryover?(set) } : all_combos
-  end
-
-  def all_combos
-    (0..9).to_a.permutation(letters.count)
+    letters.chars.zip(values).to_h
   end
 
   def valid_carryover?(set)
-    set[leading_digits.last] == 1
+    !carryover || set[leading_digits.last] == 1
   end
 
   def leading_zero?(set)
-    leading_digits.any? { |index| set[index].zero? }
+    leading_digits.any? { set[_1].zero? }
   end
 
   def valid_ones?(set)
-    ones[0..-2].map { |index| set[index] }.reduce(:+) % 10 == set[ones.last]
+    ones[0..-2].sum { set[_1] } % 10 == set[ones.last]
   end
 
   def solution?(values)
     digits = create_digits(values)
-    digits[0..-2].reduce(:+) == digits.last
+    digits[0..-2].sum == digits.last
   end
 
   def create_digits(set)
@@ -62,8 +54,4 @@ class Alphametics
   def transform(word, set)
     word.chars.map { |letter| set[letters.index(letter)] }.join.to_i
   end
-end
-
-module BookKeeping
-  VERSION = 4
 end
