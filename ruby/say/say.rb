f53120481b2daec2ce9f82734b1b8c46491f7dcb
Joe Reiff <jcreiff@gmail.com>
2020-03-30 16:14:04 -0400

    minor modifications to naming conventions

--- ruby/say/say.rb
+++ ruby/say/say.rb
@@ -1,63 +1,59 @@
 module SayArray
   refine Array do
     def clear_zeros
-      shift until first != 0
+      pop until last != 0
       self
     end
   end
 end
 
 class Say
-  MIN = 0
-  MAX = 999_999_999_999
-  SINGLE_DIGITS = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine'].freeze
-  TEENS = %w[ten eleven twelve thirteen fourteen fifteen sixteen seventeen eighteen nineteen].freeze
-  MULTIPLES_OF_TEN = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy', 'eighty', 'ninety'].freeze
-  SUFFIXES = ['', ' thousand', ' million', ' billion'].freeze
+  SINGLE_DIGITS = ['', 'one', 'two', 'three', 'four', 'five', 'six', 'seven',
+                   'eight', 'nine'].freeze
+  TEENS = %w[ten eleven twelve thirteen fourteen fifteen sixteen seventeen
+             eighteen nineteen].freeze
+  MULTIPLES_OF_TEN = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty',
+                      'seventy', 'eighty', 'ninety'].freeze
+  SUFFIXES = ['', 'thousand', 'million', 'billion'].freeze
 
   def initialize(number)
     @number = number
-    @in_range = (MIN...MAX).cover?(number)
   end
 
   def in_english
-    raise ArgumentError, 'Number out of range' unless @in_range
+    raise ArgumentError, 'Out of range' unless (0..999_999_999_999).cover?(@number)
     return 'zero' if @number.zero?
-    numbers.map.with_index { |set, i| subset(set, i) }.reverse.join(' ').strip
+    numbers.map.with_index { subset(_1, _2) }.reverse.join(' ').strip
   end
 
   private
 
   using SayArray
   def numbers
-    @number.digits.each_slice(3).to_a.map(&:reverse).map(&:clear_zeros)
+    @number.digits.each_slice(3).map { _1.clear_zeros.reverse }
   end
 
-  def subset(digits, i)
-    [to_words(digits), digits.empty? ? '' : SUFFIXES[i]].join
+  def subset(digits, power)
+    [to_words(digits), digits.empty? ? '' : SUFFIXES[power]].join(' ')
   end
 
   def to_words(digits)
-    send("write_#{digits.count}_digit", digits)
+    send("write_#{digits.count}", digits)
   end
 
-  def write_0_digit(_digits) end
+  def write_0(_digits) end
 
-  def write_1_digit(digits, i = -1)
-    SINGLE_DIGITS[digits[i]]
+  def write_1(digits, place = -1)
+    SINGLE_DIGITS[digits[place]]
   end
 
-  def write_2_digit(digits)
+  def write_2(digits)
     return TEENS[digits.join.to_i - 10] if digits.first == 1
-    divider = digits.any?(&:zero?) ? '' : '-'
-    [MULTIPLES_OF_TEN[digits.first], write_1_digit(digits)].join(divider)
+    divider = digits.last.zero? ? '' : '-'
+    [MULTIPLES_OF_TEN[digits.first], write_1(digits)].join(divider)
   end
 
-  def write_3_digit(digits)
-    [write_1_digit(digits, 0), 'hundred', write_2_digit(digits[1, 2])].join(' ')
+  def write_3(digits)
+    [write_1(digits, 0), 'hundred', write_2(digits[1, 2])].join(' ')
   end
-end
-
-module BookKeeping
-  VERSION = 1
 end
