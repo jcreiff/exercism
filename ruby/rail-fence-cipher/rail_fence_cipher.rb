fed62dcf602dfe8953b7927431c521ad5e25a5c5
Joe Reiff <jcreiff@gmail.com>
2020-03-30 12:04:32 -0400

    new solution with universal pattern

--- ruby/rail-fence-cipher/rail_fence_cipher.rb
+++ ruby/rail-fence-cipher/rail_fence_cipher.rb
@@ -1,70 +1,33 @@
-module RailFenceArray
-  refine Array do
-    def translate(other)
-      map { |i| other[i] }.join
-    end
-  end
-end
-
 class RailFenceCipher
+  def initialize(phrase, rails)
+    @phrase = phrase.chars
+    @pattern = (0..rails - 1).to_a + (1..rails - 2).to_a.reverse
+  end
+
+  def encode
+    @phrase.each_slice(@pattern.length)
+           .flat_map { _1.zip(@pattern) }
+           .group_by { _1[1] }
+           .flat_map { |_k, v| v.map { _1[0] } }
+           .join
+  end
+
+  def decode
+    (0...@phrase.length).each_slice(@pattern.length)
+                        .flat_map { _1.zip(@pattern) }
+                        .group_by { _1[1] }
+                        .flat_map { _2 }
+                        .zip(@phrase)
+                        .sort_by { _1[0] }
+                        .map { _2 }
+                        .join
+  end
+
   def self.encode(phrase, rails)
-    return phrase if invalid?(phrase, rails)
-    send("encode_#{rails}", phrase.chars, (0...phrase.length))
+    new(phrase, rails).encode
   end
 
   def self.decode(phrase, rails)
-    return phrase if invalid?(phrase, rails)
-    send("decode_#{rails}", phrase.chars)
+    new(phrase, rails).decode
   end
-
-  class << self
-    private
-
-    def invalid?(phrase, rails)
-      phrase == '' || rails > phrase.length || rails == 1
-    end
-
-    def decode_2(chars)
-      middle = (chars.length / 2.0).ceil
-      chars[0, middle].zip(chars[middle..-1]).join
-    end
-
-    def decode_3(chars)
-      top, middle, bottom = find_rails(chars, chars.length)
-      reconstruct(top, middle, bottom)
-    end
-
-    def find_rails(chars, size)
-      first = (size / 4.0).ceil
-      second = size / 2
-      third = size.even? ? size / 4 : first
-      [chars[0, first], chars[first, second], chars[-third..-1]]
-    end
-
-    def reconstruct(top, middle, bottom, i = 0, output = '')
-      segment = [top[i], bottom[i]].zip([middle[i * 2], middle[i * 2 + 1]])
-      output << segment.join
-      return output if segment.flatten.compact.length < 4
-      reconstruct(top, middle, bottom, i + 1, output)
-    end
-
-    using RailFenceArray
-    def encode_2(phrase, range)
-      range.partition(&:even?).flatten.translate(phrase)
-    end
-
-    def encode_3(phrase, range)
-      outer, second = range.partition(&:even?)
-      first, third = outer.partition { |i| (i / 2).even? }
-      [first, second, third].flatten.translate(phrase)
-    end
-
-    def encode_4(phrase, range)
-      first, third = range.select(&:even?).partition { |i| (i % 3).zero? }
-      fourth, second = range.select(&:odd?).partition { |i| (i % 3).zero? }
-      [first, second, third, fourth].flatten.translate(phrase)
-    end
-  end
-
-  VERSION = 1
 end
