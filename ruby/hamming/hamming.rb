440b9ad73c7ccaa823db0bfd2c7b726991431424
Joe Reiff <jcreiff@gmail.com>
2020-03-31 15:00:47 -0400

    updated exercism

--- ruby/hamming/hamming.rb
+++ ruby/hamming/hamming.rb
@@ -1,10 +1,6 @@
 class Hamming
-  def self.compute(strand1, strand2)
-    raise ArgumentError, 'Strands must be of equal length' if strand1.size != strand2.size
-    (0..strand1.size).count { |i| strand1[i] != strand2[i] }
+  def self.compute(str1, str2)
+    raise ArgumentError, 'Strands of unequal length' if str1.size != str2.size
+    (0..str1.size).count { str1[_1] != str2[_1] }
   end
-end
-
-module BookKeeping
-  VERSION = 3
 end
