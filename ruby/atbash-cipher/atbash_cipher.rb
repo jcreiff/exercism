12f0095693aee71144a1d18e41ce11a838cbe19c
Joe Reiff <jcreiff@gmail.com>
2020-03-31 15:40:46 -0400

    refactored to use string tr

--- ruby/atbash-cipher/atbash_cipher.rb
+++ ruby/atbash-cipher/atbash_cipher.rb
@@ -1,12 +1,10 @@
 class Atbash
+  ALPHA = ('a'..'z').to_a.join
   def self.encode(text)
-    text.downcase.scan(/[a-z0-9]/).map { |char| translate(char) }
-        .each_slice(5).map(&:join).join(' ')
+    decode(text).chars.each_slice(5).map(&:join).join(' ')
   end
 
-  def self.translate(char)
-    ALPHA.include?(char) ? ALPHA.reverse[ALPHA.index(char)] : char
+  def self.decode(text)
+    text.downcase.gsub(/\W/, '').tr(ALPHA, ALPHA.reverse)
   end
-
-  ALPHA = ('a'..'z').to_a
 end
