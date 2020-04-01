2a3a7980a077fa1105e7207f2196b633f7634f39
Joe Reiff <jcreiff@gmail.com>
2020-04-01 10:01:41 -0400

    condensed methods into one, using back reference

--- ruby/pig-latin/pig_latin.rb
+++ ruby/pig-latin/pig_latin.rb
@@ -1,18 +1,7 @@
 class PigLatin
-  REGEX = /^s?qu|^y(?!t)|(?!xr)^[^aeiouy]+/
+  REGEX = /^(s?qu|^y(?!t)|(?!xr)^[^aeiouy]+)/
 
   def self.translate(input)
-    input.split.map { |word| piglatinize(word) }.join(' ')
+    input.split.map { _1.sub(REGEX, '') << ($1 || '') << 'ay' }.join(' ')
   end
-
-  def self.piglatinize(word)
-    displaced = word.match(REGEX)
-    word.sub(REGEX, '') + displaced.to_s + 'ay'
-  end
-
-  private_class_method :piglatinize
-end
-
-module BookKeeping
-  VERSION = 2
 end
