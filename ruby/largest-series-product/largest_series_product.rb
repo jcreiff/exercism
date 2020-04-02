10c870cf470549075c14a8d3538c867ef93d18fa
Joe Reiff <jcreiff@gmail.com>
2020-04-02 13:14:35 -0400

    condensed logic

--- ruby/largest-series-product/largest_series_product.rb
+++ ruby/largest-series-product/largest_series_product.rb
@@ -1,23 +1,16 @@
 class Series
   def initialize(digits)
-    @invalid = digits.match(/\D/)
     @digits = digits.scan(/\d/).map(&:to_i)
+    @invalid = digits.match(/\D/)
   end
 
   def largest_product(range)
     return 1 if range.zero?
     check_exceptions(range)
-    @digits.each_cons(range)
-           .map { |numbers| numbers.reduce(:*) }
-           .reduce { |memo, product| memo > product ? memo : product }
+    @digits.each_cons(range).map { _1.reduce(:*) }.max
   end
 
   def check_exceptions(range)
-    raise ArgumentError, 'Invalid Series' if @invalid
-    raise ArgumentError, 'Invalid Range' unless @digits[range - 1] && range > 0
+    raise ArgumentError if @invalid || range > @digits.size
   end
-end
-
-module BookKeeping
-  VERSION = 3
 end
