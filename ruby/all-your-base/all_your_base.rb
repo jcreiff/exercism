7535674d57ffa266464c182a13d16b69ae4c15fd
Joe Reiff <jcreiff@gmail.com>
2020-04-04 19:56:27 -0400

    updated syntax, removed unnecessary guard clauses

--- ruby/all-your-base/all_your_base.rb
+++ ruby/all-your-base/all_your_base.rb
@@ -1,15 +1,13 @@
 class BaseConverter
   def self.convert(input_base, digits, output_base)
-    raise ArgumentError, 'Invalid Input' unless digits.all? { |digit| (0...input_base).cover?(digit) }
-    raise ArgumentError, 'Invalid Input' if input_base < 2 || output_base < 2
-    return digits if digits.empty?
-    return [0] if digits.all?(&:zero?)
+    raise ArgumentError unless digits.all? { (0...input_base).cover?(_1) }
+    raise ArgumentError if input_base < 2 || output_base < 2
     value = find_value(input_base, digits.reverse)
     new_digits(value, output_base)
   end
 
   def self.find_value(input_base, digits)
-    (0...digits.size).map { |i| digits[i] * (input_base**i) }.sum
+    digits.map.with_index { _1 * (input_base**_2) }.sum
   end
 
   def self.new_digits(value, output_base, digits = [])
@@ -17,8 +15,4 @@ class BaseConverter
     value, digit = value.divmod(output_base)
     new_digits(value, output_base, digits.unshift(digit))
   end
-end
-
-module BookKeeping
-  VERSION = 2
 end
