ba98a94772fab641baf7f2d1e3d20003119f3c48
Joe Reiff <jcreiff@gmail.com>
2020-04-06 14:33:20 -0400

    resolved with string tr

--- ruby/rotational-cipher/rotational_cipher.rb
+++ ruby/rotational-cipher/rotational_cipher.rb
@@ -1,18 +1,8 @@
 class RotationalCipher
-  CAPITAL = ('A'..'Z').to_a
-  LOWERCASE = ('a'..'z').to_a
+  UPPER = ('A'..'Z').to_a
+  LOWER = ('a'..'z').to_a
 
-  def self.rotate(phrase, distance)
-    phrase.chars.map { |letter| translate(letter, distance) }.join
+  def self.rotate(text, steps)
+    text.tr([UPPER, LOWER].join, [UPPER, LOWER].map { _1.rotate(steps) }.join)
   end
-
-  def self.translate(letter, distance)
-    return letter unless letter =~ /[a-zA-Z]/
-    set = letter =~ /[A-Z]/ ? CAPITAL : LOWERCASE
-    set.dup.rotate(distance)[set.index(letter)]
-  end
-end
-
-module BookKeeping
-  VERSION = 1
 end
