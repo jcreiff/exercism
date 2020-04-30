056a5c8c54f4e56154313178bbb76233dc12c0f6
Joe Reiff <jcreiff@gmail.com>
2020-04-30 12:40:43 -0400

    updating case syntax

--- ruby/darts/darts.rb
+++ ruby/darts/darts.rb
@@ -6,14 +6,10 @@ class Darts
 
   def score
     case @x**2 + @y**2
-    when 0..1
-      10
-    when 1..25
-      5
-    when 25..100
-      1
-    else
-      0
+    when 0..1 then 10
+    when 1..25 then 5
+    when 25..100 then 1
+    else 0
     end
   end
 end
